8b672e3b5eb06c29fe72f0ed294768d8fcd70eaf
Sainath Reddy <123368645+saireddie07@users.noreply.github.com>
2023-03-04 23:39:03 +0530

    Add files via upload

---
+++ spindlerbattery.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+
+
+class SpindlerBattery:
+    def needs_service(self):
+        service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 2)
+        if service_threshold_date < datetime.today().date() or self.engine_should_be_serviced():
+            return True
+        else:
+            return False
